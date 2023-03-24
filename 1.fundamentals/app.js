0d025ddb84df344336cc85ee5f18786b1d134fe9
Rafael Gomes <5102522+rafaeelneto@users.noreply.github.com>
2023-03-24 17:14:33 -0300

    update

--- 1.fundamentals/app.js
+++ 1.fundamentals/app.js
@@ -6,11 +6,15 @@ Vue.createApp({
       url: 'https://www.google.com',
       raw_url:
         '<a href="https://www.google.com" target="_blank">Google link raw</a>',
+      age: 20,
     };
   },
   methods: {
     fullname() {
       return `${this.firstName} ${this.lastName}`;
+    },
+    increment() {
+      this.age++;
     },
   },
 }).mount('#app');
