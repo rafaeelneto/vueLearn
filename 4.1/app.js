0f86ca582c98ecfff8d9e3ed8896400db68d4f48
Rafael Neto <5102522+rafaeelneto@users.noreply.github.com>
2023-04-19 18:15:21 -0300

    feat: vue compiler lesson

--- 4.1/app.js
+++ 4.1/app.js
@@ -1,7 +1,53 @@
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello world!"
-    }
-  }
-}).mount('#app')
+      message: 'Hello world!',
+    };
+  },
+  template: `{{message}}`,
+  beforeCreate() {
+    console.log(`beforeCreate function called ${this.message}`);
+  },
+  created() {
+    console.log(`creted() function called. message ${this.message}`);
+  },
+  beforeMount() {
+    console.log(`beforeMount() function called: ${this.$el}`);
+    // $el is the element that vue is assinged to
+  },
+  mounted() {
+    console.log(`mounted() function called.`, this.$el);
+  },
+  beforeUpdate() {
+    console.log(`beforeUpdate() function called.`);
+  },
+  updated() {
+    console.log(`updated() function called.`);
+  },
+  beforeUnmount() {
+    console.log(`beforeUnmount() function called.`);
+  },
+  unmounted() {
+    console.log(`unmounted() function called.`);
+  },
+});
+
+vm.mount('#app');
+
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: 'Hello world!',
+    };
+  },
+  render() {
+    return Vue.h('h1', this.message);
+  },
+}).mount('#app2');
+
+// ===============================
+// You can mount the vue application any time in the runtime
+// ===============================
+// setTimeout(() => {
+//   vm.mount('#app');
+// }, 3000);
