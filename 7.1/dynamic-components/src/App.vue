55afb80fdc522843bd3bbeed6958862ec9a91190
Rafael Neto <5102522+rafaeelneto@users.noreply.github.com>
2023-04-25 15:08:30 -0300

    feat: add dynamic components code lesson

---
+++ 7.1/dynamic-components/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3>dynamic componets</h3>
+  <select v-model="componentName">
+    <option default value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+
+  <p>
+    when a component is tongled out it gets unmounted, so any data will not be
+    available anymore and will be reset to its default state
+  </p>
+  <component :is="componentName"></component>
+  <p>
+    the tag keep-alive keeps the component removed on memory so the data remains
+  </p>
+  <KeepAlive>
+    <component :is="componentName"></component>
+  </KeepAlive>
+</template>
+
+<script>
+import About from './components/About.vue';
+import Home from './components/Home.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    About,
+  },
+  data() {
+    return {
+      componentName: 'Home',
+    };
+  },
+};
+</script>
---
+++ 7.1/dynamic-components/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  unmounted() {
+    console.log('Home component unmounted');
+  },
+  activated() {
+    console.log('Home component activated');
+  },
+  deactivated() {
+    console.log('Home component deactivated');
+  },
+};
+</script>
